18e0bd3d395b2ac3a02a94f71c2e0a8e43a5f61e
N Murali <113176341+nagarimurali@users.noreply.github.com>
2024-04-06 16:06:27 +0530

    UseRefHookEx2

--- src/ReactHooks/UseRefHook/UseRefHookEx.js
+++ src/ReactHooks/UseRefHook/UseRefHookEx.js
@@ -1,8 +1,32 @@
 import React from 'react'
-import {useState,useEffect,useRef} from 'react'
+import { useState, useRef } from 'react'
 
 const UseRefHookEx = () => {
-    const[name,setName]=useState("");
+    const [name, setName] = useState("");
+    const renderCount = useRef("")
+
+    //     useEffect(()=>{
+    //  renderCount.current=renderCount.current+1
+
+    //     })
+
+    const focusFun = () => {
+        renderCount.current.focus()
+    }
+    return (
+        <div style={{ marginTop: "5px" }}>
+            <input ref={renderCount} type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+            <h1>Type:{name}</h1>
+            <button onClick={focusFun}>Focus</button>
+        </div>
+    )
+}
+
+export default UseRefHookEx
+
+/*
+
+   const[name,setName]=useState("");
     const renderCount=useRef(1)
 
     useEffect(()=>{
@@ -17,6 +41,4 @@ const UseRefHookEx = () => {
      <h1>Component {renderCount.current} times Rendering</h1>
     </div>
   )
-}
-
-export default UseRefHookEx
+*/
