4f2e2c5549a6593ebc2d9ae3cd2f550da6db5886
N Murali <113176341+nagarimurali@users.noreply.github.com>
2024-04-07 17:28:38 +0530

    Lazy loading

--- src/Routers/components/RouterApp.js
+++ src/Routers/components/RouterApp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
-import About from './About';
+// import About from './About';
 import Contact from './Contact';
 import NotFond from './NotFond';
 import Success from './Success';
@@ -12,6 +12,8 @@ import FetureProject from './FetureProject';
 import User from './DynamicRouter/User';
 import UserDetails from './DynamicRouter/UserDetails';
 
+const LazyAbout = React.lazy(() => import('./About'))
+
 
 const RouterApp = () => {
     return (
@@ -20,16 +22,20 @@ const RouterApp = () => {
             <Routes>
 
                 <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
+                <Route path='/about' element={
+                    <React.Suspense fallback='Loading..'>
+                        <LazyAbout />
+                    </React.Suspense>
+                } />
                 <Route path='/contact' element={<Contact />} />
                 <Route path='/success' element={<Success />} />
                 <Route path='/project' element={<Project />}>
-                    <Route index element={<FetureProject/>}/>
+                    <Route index element={<FetureProject />} />
                     <Route path='feture' element={<FetureProject />} />
-                    <Route path='new' element={<NewProject/>}/>
+                    <Route path='new' element={<NewProject />} />
                 </Route>
-                <Route path='/users'element={<User/>}/>
-                <Route path='/users/:userId' element={<UserDetails/>}/>
+                <Route path='/users' element={<User />} />
+                <Route path='/users/:userId' element={<UserDetails />} />
                 <Route path='*' element={<NotFond />} />
             </Routes>
 
